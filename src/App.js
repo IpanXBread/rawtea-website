04bf059c79d72ad52338fe3f714d525943455dcd
IpanXBread <ipanxbread@gmail.com>
2024-03-05 18:20:14 +0800

    2nd commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Raw Tea - Minecraft Server Official Website
+          Raw Tea - Minecraft Server Official Websitessss
         </p>
         <a
           className="App-link"
